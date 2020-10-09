29339ab596ade6cc3c3b4a9d23737378928ba06b
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-09 19:49:51 +0545

    -updated bording screen

--- lib/screen/OnBoardingScreen.dart
+++ lib/screen/OnBoardingScreen.dart
@@ -7,7 +7,7 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
-  final int _totalPages = 3;
+  final int _totalPages = 4;
   final PageController _pageController = PageController(initialPage: 0);
   int _currentPage = 0;
 
@@ -27,30 +27,31 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                 _buildPageContent(
                   image: "assets/netflix-1.jpg",
                   body: "Trying to \n join Netflix?",
-                    desc:"You can sign up for Netflix in \n the app. We know it's hassle. \n After you're a member, you \ncant start  watching in the app.",
-                  color: Color.fromRGBO(83,244,21, 1),
+                  desc:
+                      "You can sign up for Netflix in \n the app. We know it's hassle. \n After you're a member, you \ncant start  watching in the app.",
+                  color: Color.fromRGBO(83, 244, 21, 1),
                 ),
                 _buildPageContent(
                   image: "assets/netflix-2.png",
                   body: "Watch on \n any device?",
-                  desc:"Stream on your phone, tablet, laptop, and TV",
+                  desc: "Stream on your phone, tablet, laptop, and TV",
                   color: Color.fromRGBO(255, 141, 104, 1),
                 ),
                 _buildPageContent(
                   image: "assets/netflix-3.png",
                   body: "3,2,1...Download!",
-                  desc:"Always have something to watch offline.",
+                  desc: "Always have something to watch offline.",
                   color: Color.fromRGBO(195, 169, 255, 1),
                 ),
                 _buildPageContent(
                   image: "assets/netflix-4.png",
                   body: "No pesky contracts.",
-                  desc:"Cancel anytime.",
+                  desc: "Cancel anytime.",
                   color: Color.fromRGBO(130, 172, 38, 1),
                 ),
               ],
             ),
-                //Header
+            //Header
             Positioned(
               top: 5,
               left: MediaQuery.of(context).size.width * .03,
@@ -69,20 +70,32 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                           ),
                           Spacer(),
                           InkWell(
-                            onTap: (){},
+                            onTap: () {},
                             child: Container(
-                              alignment:Alignment.center,
+                              alignment: Alignment.center,
                               child: Row(
                                 crossAxisAlignment: CrossAxisAlignment.end,
                                 mainAxisAlignment: MainAxisAlignment.end,
                                 children: [
                                   Padding(
                                     padding: EdgeInsets.only(right: 10),
-                                    child: Text("Help",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),),
+                                    child: Text(
+                                      "Help",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15),
+                                    ),
                                   ),
                                   Padding(
                                     padding: EdgeInsets.only(right: 10),
-                                    child: Text("Privacy",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),),
+                                    child: Text(
+                                      "Privacy",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15),
+                                    ),
                                   )
                                 ],
                               ),
@@ -93,6 +106,37 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                     )
                   ],
                 ),
+              ),
+            ),
+            /*indicator onboarding screen*/
+            Positioned(
+              bottom: 10,
+              left: MediaQuery.of(context).size.width * .03,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width * .9,
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          for (int i = 0; i < _totalPages; i++)
+                            i == _currentPage
+                                ? _buildPageIndicator(true)
+                                : _buildPageIndicator(false)
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  Container(
+                    alignment: Alignment.center,
+                  )
+                ],
               ),
             )
           ],
@@ -106,28 +150,46 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
     return Container(
       decoration: BoxDecoration(
           image: DecorationImage(
-              image: AssetImage(image,),
-              fit: BoxFit.cover
-          )
-      ),
+              image: AssetImage(
+                image,
+              ),
+              fit: BoxFit.cover)),
       padding: EdgeInsets.symmetric(horizontal: 40),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
-            padding: EdgeInsets.only(top:200),
-            child: Text(body,textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 35,fontWeight: FontWeight.w800,color: color),
+            padding: EdgeInsets.only(top: 200),
+            child: Text(
+              body,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 35, fontWeight: FontWeight.w800, color: color),
             ),
           ),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
           Text(
-            desc,textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20,height: 1.2,color: Colors.white),
+            desc,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, height: 1.2, color: Colors.white),
           )
         ],
       ),
+    );
+  }
+
+  Widget _buildPageIndicator(bool isCurrentPage) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 350),
+      margin: EdgeInsets.symmetric(horizontal: 4.0),
+      height: 7.5,
+      width: 7.5,
+      decoration: BoxDecoration(
+          color: isCurrentPage ? Colors.red : Colors.white54,
+          borderRadius: BorderRadius.all(Radius.circular(12))),
     );
   }
 }
