d75913e3cfc2b6169e109397b8ccbdaadaaf743a
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-09 23:22:43 +0545

    -sign in screen bug fixed

--- lib/screen/SignInScreen.dart
+++ lib/screen/SignInScreen.dart
@@ -1,28 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignInScreen extends StatefulWidget {
-  SignInScreen({Key key}):super(key:key);
+  SignInScreen({Key key}) : super(key: key);
+
   @override
   _SignInScreenState createState() => _SignInScreenState();
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-  final textControllerUser=TextEditingController(),
-  textControllerPassword=TextEditingController();
+  final textControllerUser = TextEditingController(),
+      textControllerPassword = TextEditingController();
 
   bool isCheck = false;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
+    textControllerUser.addListener(_printLatestValue);
+    textControllerPassword.addListener(_printLatestValue);
   }
 
-  _printLatestValue(){
+  _printLatestValue() {
     setState(() {
-      if(textControllerUser.text.length>0 && textControllerPassword.text.length>0){
-        isCheck=true;
-      }else if(textControllerUser.text.length==0 || textControllerPassword.text.length==0){
-        isCheck=false;
+      if (textControllerUser.text.length > 0 &&
+          textControllerPassword.text.length > 0) {
+        isCheck = true;
+      } else if (textControllerUser.text.length == 0 ||
+          textControllerPassword.text.length == 0) {
+        isCheck = false;
       }
     });
   }
@@ -36,17 +42,155 @@ class _SignInScreenState extends State<SignInScreen> {
         centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
-          onPressed: ()=>Navigator.of(context).pop(),
+          onPressed: () => Navigator.of(context).pop(),
         ),
         actions: [
           Padding(
-            padding: EdgeInsets.only(right: 20,top: 20),
-            child: Text("Help",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 12),),
+            padding: EdgeInsets.only(right: 20, top: 20),
+            child: Text(
+              "Help",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12),
+            ),
           )
         ],
         elevation: 0,
       ),
-      body: Center(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, right: 40, left: 40),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(48, 45, 45, 1),
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  child: TextFormField(
+                    controller: textControllerUser,
+                    style: TextStyle(color: Colors.white),
+                    cursorColor: Colors.white,
+                    decoration: InputDecoration(
+                      focusColor: Colors.white,
+                      labelText: "Email or Phone number",
+                      labelStyle: TextStyle(color: Colors.white),
+                      contentPadding: EdgeInsets.all(10),
+                      fillColor: Colors.white,
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, right: 40, left: 40),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(48, 45, 45, 1),
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  child: TextFormField(
+                    controller: textControllerPassword,
+                    style: TextStyle(color: Colors.white),
+                    cursorColor: Colors.white,
+                    decoration: InputDecoration(
+                      focusColor: Colors.white,
+                      labelText: "Password",
+                      labelStyle: TextStyle(color: Colors.white),
+                      contentPadding: EdgeInsets.all(10),
+                      fillColor: Colors.white,
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent)),
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width * .80,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      color: (isCheck)
+                          ? Color.fromRGBO(213, 0, 2, 1)
+                          : Colors.transparent,
+                      borderRadius: BorderRadius.all(Radius.circular(2))),
+                  child: Center(
+                    child: InkWell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "SIGN IN",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width * .80,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(2))),
+                  child: Center(
+                    child: InkWell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Recover Password",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width * .80,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(2))),
+                  child: Center(
+                    child: RichText(
+                        text: TextSpan(
+                            style:
+                                TextStyle(fontSize: 14, color: Colors.black),
+                  children:<TextSpan>[
+                    TextSpan(text: "Sign in is protected by Google reCAPTCHA to ensure you're not a bot.",style: TextStyle(color: Colors.grey)),
+                    TextSpan(text: "Learn More",style: TextStyle(fontWeight: FontWeight.bold,color:Colors.grey ))
+                  ]
+                        ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
