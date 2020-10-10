b0d194af9024b4e30a95a22fd2ffe2ba4b03c361
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-10 18:16:50 +0545

    -added main view layout

--- lib/screen/MainScreen.dart
+++ lib/screen/MainScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -124,17 +125,86 @@ class _MainScreenState extends State<MainScreen> {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage("assets/netflix-movie4.jpg"),
-          fit: BoxFit.cover,
+          image: AssetImage("assets/stranger_thing.jpg",),
+
         )
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Container(),
           Padding(
-            padding: EdgeInsets.only(top: 250),
-            child: Container(),
+            padding: EdgeInsets.only(top: 300),
+            child: Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width * 1,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(2))
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/netflix_logo.jpg",height: 30,width: 30,),
+                      Text("SERIES",style: TextStyle(color:Colors.grey),)
+                    ],
+                  ),
+                  Text("STRANGER\n THINGS",textAlign: TextAlign.center,
+                  style:TextStyle(
+                    fontSize: 35,
+                    color: Colors.white,
+                  ) ,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("Ominous",style: TextStyle(color: Colors.grey)),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10,right:10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            color: Colors.red,
+                          ),
+                          height: 5,
+                          width: 5,
+                        ),
+                      ),
+                      Text("Exciting",style: TextStyle(color: Colors.grey)),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10,right:10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            color: Colors.red,
+                          ),
+                          height: 5,
+                          width: 5,
+                        ),
+                      ),
+                      Text("Teen",style: TextStyle(color: Colors.grey)),
+
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10,left: 25,right: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(Icons.add,color: Colors.white,),
+                        Text("My List" ,style: TextStyle(color: Colors.grey,fontSize: 14,fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+            ),
           )
         ],
       ),
