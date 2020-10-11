7909d66c4a5679774267786243f0f2061df8b3cd
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-11 23:26:53 +0545

    -appbar error fix on main page

--- lib/screen/MainScreen.dart
+++ lib/screen/MainScreen.dart
@@ -15,9 +15,12 @@ class _MainScreenState extends State<MainScreen> {
   List imagePreview=[
     {"image":"assets/netflix-movie1.jpg","title":"Imperial Dreams","color":Colors.orangeAccent},
     {"image":"assets/netflix-movie2.jpg","title":"Quick Sands","color":Colors.white},
-    {"image":"assets/netflix-movie3.jpg","title":"The Haunting","color":Colors.grey},
+    {"image":"assets/netflix-movie3.jpg","title":"The Haunting","color":Colors.greenAccent},
     {"image":"assets/netflix-movie4.jpg","title":"The Irishman","color":Colors.orangeAccent},
-    {"image":"assets/netflix-movie5.jpg","title":"Extention","color":Colors.red},
+    {"image":"assets/netflix-movie5.jpg","title":"Extinction","color":Colors.redAccent},
+    {"image":"assets/netflix-movie6.jpg","title":"Annihilation","color":Colors.white},
+    {"image":"assets/netflix-movie7.jpg","title":"Tall Girls","color":Colors.blueAccent},
+    {"image":"assets/netflix-movie8.jpg","title":"Old Guard","color":Colors.red},
   ];
 
   @override
@@ -137,25 +140,29 @@ class _MainScreenState extends State<MainScreen> {
   Widget headerMenu(){
     return Positioned(
       child: AppBar(
+
         backgroundColor: Colors.transparent,
         centerTitle: true,
         leading: Container(
-          child:Image.asset("assets/netflix_logo.png",height: 0,width: 20,),
-
+          child:Column(
+            children: [
+              Image.asset("assets/netflix_logo.png",height: 45,),
+            ],
+          ),
         ),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Padding(
-              padding: EdgeInsets.only(right: 20,top: 20),
+              padding: EdgeInsets.only(right: 20,top: 5),
               child: Text("TV Programmes",style: TextStyle(color: Colors.white,fontSize: 15),),
             ),
             Padding(
-              padding: EdgeInsets.only(right: 20,top: 20),
+              padding: EdgeInsets.only(right: 20,top: 5),
               child: Text("Films",style: TextStyle(color: Colors.white,fontSize: 15),),
             ),
             Padding(
-              padding: EdgeInsets.only(right: 20,top: 20),
+              padding: EdgeInsets.only(right: 20,top: 5),
               child: Text("My List",style: TextStyle(color: Colors.white,fontSize: 15),),
             ),
           ],
@@ -218,7 +225,7 @@ class _MainScreenState extends State<MainScreen> {
                           width: 5,
                         ),
                       ),
-                      Text("Myster",style: TextStyle(color: Colors.white70)),
+                      Text("Mystery",style: TextStyle(color: Colors.white70)),
                       Padding(
                         padding: EdgeInsets.only(left: 10,right:10),
                         child: Container(
@@ -332,19 +339,19 @@ class _MainScreenState extends State<MainScreen> {
                       child: Align(
                         alignment: FractionalOffset.bottomCenter,
                         child: Container(
-                          height: 20,
+                          height: 17,
                           width: 100,
                           decoration: BoxDecoration(
                             boxShadow: [
                               BoxShadow(
                                 color: Colors.black.withOpacity(0.8),
-                                spreadRadius: 10,
-                                blurRadius: 5,
+                                spreadRadius: 12,
+                                blurRadius: 7,
                                 offset: Offset(0,5),
                               ),
                             ],
                           ),
-                          child: Text(imagePreview[index]["title"],style: TextStyle(color: Colors.white),),
+                          child: Text(imagePreview[index]["title"],style: TextStyle(color: imagePreview[index]["color"],fontWeight: FontWeight.bold,fontSize: 15),textAlign: TextAlign.center,),
                         ),
                       ),
                     )
