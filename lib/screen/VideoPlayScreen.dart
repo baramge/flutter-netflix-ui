ad4399371aea3f64c83aad3d6b3495c611afb189
baramge <lunesea@naver.com>
2022-04-29 15:56:24 +0900

    Update VideoPlayScreen.dart
    gravity 영상 연결

--- lib/screen/VideoPlayScreen.dart
+++ lib/screen/VideoPlayScreen.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -72,12 +73,16 @@ class VideoPlayScreenStatus extends State<VideoPlayScreen> {
     // _controller = VideoPlayerController.file(file);
 
     // todo : picker는 초기 1회만 얻어오고 이후로는 결과만 가지고 재생
-    String filename = "/data/user/0/com.example.netflix_ui/cache/file_picker/[VR] ITZY 'WANNABE' VR CAM │@SBS Inkigayo_2020.4.5 [0rDz0x1mCb4].webm";
+    // String filename = "/data/user/0/com.example.netflix_ui/cache/file_picker/[VR] ITZY 'WANNABE' VR CAM │@SBS Inkigayo_2020.4.5 [0rDz0x1mCb4].webm";
+    String filename = "/data/user/0/com.example.netflix_ui/cache/file_picker/Gravity.2013.1080p.3D.BluRay.Half-SBS.x264.TrueHD.7.1.Atmos-RARBG.mp4";
     _controller = VideoPlayerController.file(File(filename));
 
     await Future.wait( [_controller.initialize()] );
     setState(() {
-      _controller.play();
+      Duration position = Duration( hours: 1,minutes: 11, seconds: 50 );
+      // Duration position = Duration( minutes: 11, seconds: 30 );
+      _controller.seekTo(position).then((value) => _controller.play());
+      // _controller.play();
     });
   }
 
